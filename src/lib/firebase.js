eef76ca32bf6ddc2f70ac2e22af1384e3d762b79
H_Ghurbani <52192905+HGhurbani@users.noreply.github.com>
2025-08-11 12:23:31 +0300

    feat: store images on firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,5 +22,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { app, analytics, db, auth }; 
+export { app, analytics, db, auth, storage };
